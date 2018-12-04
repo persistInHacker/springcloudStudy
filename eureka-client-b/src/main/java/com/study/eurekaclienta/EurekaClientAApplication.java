8b21ce497f4220cc0e06fdb5af7ce7dd7c86ca4a
persistInHacker <874387886@qq.com>
2018-12-04 14:07:54 +0800

    first commit

---
+++ eureka-client-b/src/main/java/com/study/eurekaclienta/EurekaClientAApplication.java
@@ -0,0 +1,12 @@
+package com.study.eurekaclienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientAApplication.class, args);
+	}
+}
