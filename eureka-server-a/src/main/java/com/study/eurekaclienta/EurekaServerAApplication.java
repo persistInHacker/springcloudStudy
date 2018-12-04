68bce9fd713c8cc485a39f1c18e3e23dbc2b4ac7
persistInHacker <874387886@qq.com>
2018-12-04 14:20:14 +0800

    demo

--- eureka-server-a/src/main/java/com/study/eurekaclienta/EurekaServerAApplication.java
+++ eureka-server-a/src/main/java/com/study/eurekaclienta/EurekaServerAApplication.java
@@ -2,11 +2,13 @@ package com.study.eurekaclienta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaClientAApplication {
+@EnableEurekaServer
+public class EurekaServerAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientAApplication.class, args);
+		SpringApplication.run(EurekaServerAApplication.class, args);
 	}
 }
